691e1e735b7e8364066a4bdf5616ee54493b7f1a
Muhammad Adinata <mail.dieend@gmail.com>
2013-09-11 12:29:39 +0700

    Removed dummy data
    Removed dummy data from sql dump

--- dragon_ball.sql
+++ dragon_ball.sql
@@ -36,39 +36,6 @@ CREATE TABLE IF NOT EXISTS `ball` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `ball`
---
-
-INSERT INTO `ball` (`id`, `bssid`, `latitude`, `longitude`, `validity`, `wifi_signal`) VALUES
-('1', 'amazing_race', 0, 0, 2, 0),
-('10', 'amazing_race', 0, 0, 2, 0),
-('11', 'amazing_race', 0, 0, 2, 0),
-('12', 'amazing_race', 0, 0, 2, 0),
-('13', 'amazing_race', 0, 0, 2, 0),
-('14', 'amazing_race', 0, 0, 2, 0),
-('15', 'amazing_race', 0, 0, 2, 0),
-('16', 'amazing_race', 0, 0, 2, 0),
-('17', 'amazing_race', 0, 0, 2, 0),
-('18', 'amazing_race', 0, 0, 2, 0),
-('19', 'amazing_race', 0, 0, 2, 0),
-('2', 'amazing_race', 0, 0, 2, 0),
-('20', 'amazing_race', 0, 0, 2, 0),
-('21', 'amazing_race', 39.737755555556, 104.98928611111, 0, 0),
-('296c91a3fe51ad54413124a99657144b', 'qwertb', -6.887796, 107.60855, 1, 0),
-('2f6dbc1b5be83963f671a6220a2c57ab', 'qwertj', -6.887796, 107.60855, 1, 0),
-('3', 'amazing_race', 0, 0, 2, 0),
-('4', 'amazing_race', 0, 0, 2, 0),
-('5', 'amazing_race', 0, 0, 2, 0),
-('6', 'amazing_race', 0, 0, 2, 0),
-('7', 'amazing_race', 0, 0, 2, 0),
-('8', 'amazing_race', 0, 0, 2, 0),
-('9', 'amazing_race', 0, 0, 2, 0),
-('994f5f08564fe2935918936661535e69', 'qwertu', -6.887796, 107.60855, 1, 0),
-('ae9ecddbd4dc36e9cd6101917031d3eb', 'qwertl', -6.887796, 107.60855, 1, 0),
-('f08b939dccf3bf386dcb16f48b38a2f2', 'qwertn', -6.887796, 107.60855, 1, 0),
-('f1660e6da6f6d46e17515ef0230a47ad', 'qwerte', -6.887796, 107.60855, 1, 0),
-('f9e22bda3a98225286a227c35978c863', 'qwertp', -6.887796, 107.60855, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -85,32 +52,6 @@ CREATE TABLE IF NOT EXISTS `ball_info` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
---
--- Dumping data for table `ball_info`
---
-
-INSERT INTO `ball_info` (`id`, `bssid`, `latitude`, `longitude`,`wifi_signal`) VALUES
-(1, 'qwerta', -6.887796, 107.60855, 0),
-(2, 'qwertb', -6.887796, 107.60855, 0),
-(3, 'qwertc', -6.887796, 107.60855, 0),
-(4, 'qwertd', -6.887796, 107.60855, 0),
-(5, 'qwerte', -6.887796, 107.60855, 0),
-(6, 'qwertf', -6.887796, 107.60855, 0),
-(7, 'qwertg', -6.887796, 107.60855, 0),
-(8, 'qwerth', -6.887796, 107.60855, 0),
-(9, 'qwerti', -6.887796, 107.60855, 0),
-(10, 'qwertj', -6.887796, 107.60855, 0),
-(11, 'qwertk', -6.887796, 107.60855, 0),
-(12, 'qwertl', -6.887796, 107.60855, 0),
-(13, 'qwertm', -6.887796, 107.60855, 0),
-(14, 'qwertn', -6.887796, 107.60855, 0),
-(15, 'qwerto', -6.887796, 107.60855, 0),
-(16, 'qwertp', -6.887796, 107.60855, 0),
-(17, 'qwertq', -6.887796, 107.60855, 0),
-(18, 'qwertr', -6.887796, 107.60855, 0),
-(19, 'qwerts', -6.887796, 107.60855, 0),
-(20, 'qwertt', -6.887796, 107.60855, 0),
-(21, 'qwertu', -6.887796, 107.60855, 0);
 
 -- --------------------------------------------------------
 
@@ -146,18 +87,6 @@ CREATE TABLE IF NOT EXISTS `group_ball` (
   KEY `group_id` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `group_ball`
---
-
-INSERT INTO `group_ball` (`group_id`, `ball_id`) VALUES
-('f9cf0876055ebd0b831a68fa0a8cc61f', 'db30307ddafd40a22864fdebf39ba3a3'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', 'da9ff6ac031efdcd8d35821e77b48997'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', 'da7cba409de6224148f624ca49a0b95c'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', 'a67afcbff5ec51fe6671d558038b8673'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', '89951feae513e7dd325dcbab8e687720'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', '44a87aa8e7a80b29550b7c9b63751c44'),
-('f9cf0876055ebd0b831a68fa0a8cc61f', '619b0007268ec481f600b57da0bad456');
 
 -- --------------------------------------------------------
 
